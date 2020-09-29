94083d8fd015e602da9eb74b6eadd90183382086
Jiawei Du <jiadu@microsoft.com>
2020-09-29 11:40:49 +0800

    add azs k8s jump start

---
+++ azure_arc_data_jumpstart/azsaks/arm_template/scripts/ClientTools.ps1
@@ -0,0 +1,171 @@
+param (
+    [string]$servicePrincipalClientId,
+    [string]$servicePrincipalClientSecret,
+    [string]$adminUsername,
+    [string]$tenantId,
+    [string]$clusterName,
+    [string]$resourceGroup,
+    [string]$AZDATA_USERNAME,
+    [string]$AZDATA_PASSWORD,
+    [string]$ACCEPT_EULA,
+    [string]$REGISTRY_USERNAME,
+    [string]$REGISTRY_PASSWORD,
+    [string]$ARC_DC_NAME,
+    [string]$ARC_DC_SUBSCRIPTION,
+    [string]$ARC_DC_REGION,
+    [string]$chocolateyAppList,
+    [string]$DOCKER_REGISTRY,
+    [string]$DOCKER_REPOSITORY,
+    [string]$DOCKER_TAG
+)
+
+[System.Environment]::SetEnvironmentVariable('servicePrincipalClientId', $servicePrincipalClientId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('servicePrincipalClientSecret', $servicePrincipalClientSecret,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('tenantId', $tenantId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('clusterName', $clusterName,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('AZDATA_USERNAME', $AZDATA_USERNAME,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('AZDATA_PASSWORD', $AZDATA_PASSWORD,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('ACCEPT_EULA', $ACCEPT_EULA,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('REGISTRY_USERNAME', $REGISTRY_USERNAME,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('REGISTRY_PASSWORD', $REGISTRY_PASSWORD,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('ARC_DC_NAME', $ARC_DC_NAME,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('ARC_DC_SUBSCRIPTION', $ARC_DC_SUBSCRIPTION,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('ARC_DC_REGION', $ARC_DC_REGION,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('DOCKER_REGISTRY', $DOCKER_REGISTRY,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('DOCKER_REPOSITORY', $DOCKER_REPOSITORY,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('DOCKER_TAG', $DOCKER_TAG,[System.EnvironmentVariableTarget]::Machine)
+
+# Installing tools
+New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
+workflow ClientTools_01
+        {
+            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli'
+            #Run commands in parallel.
+            Parallel 
+                {
+                    InlineScript {
+                        param (
+                            [string]$chocolateyAppList
+                        )
+                        if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false)
+                        {
+                            try{
+                                choco config get cacheLocation
+                            }catch{
+                                Write-Output "Chocolatey not detected, trying to install now"
+                                iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+                            }
+                        }
+                        if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false){   
+                            Write-Host "Chocolatey Apps Specified"  
+                            
+                            $appsToInstall = $using:chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+                        
+                            foreach ($app in $appsToInstall)
+                            {
+                                Write-Host "Installing $app"
+                                & choco install $app /y -Force| Write-Output
+                            }
+                        }                        
+                    }
+                    Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=2142211" -OutFile "C:\tmp\azuredatastudio.zip"
+                    Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/settings.json" -OutFile "C:\tmp\settings.json"
+                    Invoke-WebRequest "https://aka.ms/azdata-msi" -OutFile "C:\tmp\AZDataCLI.msi"
+                    Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/scripts/DC_Cleanup.ps1" -OutFile "C:\tmp\DC_Cleanup.ps1"
+                    Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/scripts/DC_Deploy.ps1" -OutFile "C:\tmp\DC_Deploy.ps1"
+                }
+        }
+
+ClientTools_01 | ft
+
+workflow ClientTools_02
+        {
+            #Run commands in parallel.
+            Parallel
+            {
+                InlineScript {
+                    Expand-Archive C:\tmp\azuredatastudio.zip -DestinationPath 'C:\Program Files\Azure Data Studio'
+                    Start-Process msiexec.exe -Wait -ArgumentList '/I C:\tmp\AZDataCLI.msi /quiet'
+                }
+            }
+        }
+        
+ClientTools_02 | ft 
+
+New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
+New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
+
+# Creating Powershell Logon Script
+$LogonScript = @'
+Start-Transcript -Path C:\tmp\LogonScript.log
+
+<#
+$azurePassword = ConvertTo-SecureString $env:servicePrincipalClientSecret -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($env:servicePrincipalClientId , $azurePassword)
+Connect-AzAccount -Credential $psCred -TenantId $env:tenantId -ServicePrincipal
+Import-AzAksCredential -ResourceGroupName $env:resourceGroup -Name $env:clusterName -Force
+#>
+
+kubectl get nodes
+azdata --version
+
+Write-Host "Installing Azure Data Studio Extensions"
+Write-Host "`n"
+
+$env:argument1="--install-extension"
+$env:argument2="Microsoft.arc"
+$env:argument3="microsoft.azuredatastudio-postgresql"
+
+& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument2
+& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument3
+
+Write-Host "Copying Azure Data Studio Settings Config"
+$SettingsDestination = "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User"
+Start-Process -FilePath "C:\Program Files\Azure Data Studio\azuredatastudio.exe" -WindowStyle Hidden
+Start-Sleep -s 5
+Stop-Process -Name "azuredatastudio" -Force
+Copy-Item -Path "C:\tmp\settings.json" -Destination $SettingsDestination -Recurse -Force -ErrorAction Continue
+
+Write-Host "Creating Azure Data Studio Desktop shortcut"
+Write-Host "`n"
+$TargetFile = "C:\Program Files\Azure Data Studio\azuredatastudio.exe"
+$ShortcutFile = "C:\Users\$env:adminUsername\Desktop\Azure Data Studio.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+# Deploying Azure Arc Data Controller
+start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
+azdata arc dc config init --source azure-arc-aks-premium-storage --path ./custom
+if(($env:DOCKER_REGISTRY -ne $NULL) -or ($env:DOCKER_REGISTRY -ne ""))
+{
+    azdata arc dc config replace --path ./custom/control.json --json-values "spec.docker.registry=$env:DOCKER_REGISTRY"
+}
+if(($env:DOCKER_REPOSITORY -ne $NULL) -or ($env:DOCKER_REPOSITORY -ne ""))
+{
+    azdata arc dc config replace --path ./custom/control.json --json-values "spec.docker.repository=$env:DOCKER_REPOSITORY"
+}
+if(($env:DOCKER_TAG -ne $NULL) -or ($env:DOCKER_TAG -ne ""))
+{
+    azdata arc dc config replace --path ./custom/control.json --json-values "spec.docker.imageTag=$env:DOCKER_TAG"
+}
+
+azdata arc dc create --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:resourceGroup --location $env:ARC_DC_REGION --connectivity-mode indirect --path ./custom
+
+Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$false
+
+Stop-Transcript
+
+Stop-Process -name powershell -Force
+'@ > C:\tmp\LogonScript.ps1
+
+# Creating LogonScript Windows Scheduled Task
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
+Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User $adminUsername -Action $Action -RunLevel "Highest" -Force
+
+# Disabling Windows Server Manager Scheduled Task
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
---
+++ azure_arc_data_jumpstart/azsaks/arm_template/scripts/deployK8SbyAKSe.ps1
@@ -0,0 +1,103 @@
+[CmdletBinding()]
+param (
+    [string] $StampLocation="shanghai",
+    [string] $OrchestratorRelease="1.17",
+    [string] $OrchestratorVersion="1.17.11",
+    [string] $PortalURL="https://portal.shanghai.int.azurestack.corp.microsoft.com/",
+    [string] $IdentitySystem="azure_ad",
+    [string] $DnsPrefix="k8s-jiadutest1",
+    [string] $K8SAdminUser="azureuser",
+    [string] $K8SPublicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbYi6m97Dtl9vcy7L8KPuzs1iU8hAAvAQp+DQsh1YzhGJL1oh5wZcqrKiUOQGfjpY7VEOWW6Boarv7Jc0aeB9sZzzkpFj3nDrucytOYeA9HBXY43aCofRkrY69+6YGjEwqtMoqZsvqXe8wpZf+Kl4zy4WOrETWS4JUQNjk1h0UOrcZGPbbltZKS73kJtD5RKEV0tne9oBGRqN/xaXbutSDkSnbNCqoPUfqnO1p0hn4po+J6BMPEYlzz6SD6rPzlYMSyk+MX2kh0TEgZHxubhoTpYq2vSkK+7fP235FRpt+5BRz1Mpom0APhmsjjoLGTmGx+kaSUab2u7CPDNy0+rB7 fareast\jiadu@jiadu-dev1",
+    [string] $K8SPClientId="a154f21f-1c5f-4da5-867b-22f7aa5afcc4",
+    [string] $K8SSPSecret="9Bw4.25O96K6A35pRegG.CqsIP1~0ida6~",
+    [string] $AzureEnv="AzureStackCloud",
+    [string] $AzsK8SSubscriptionId="5b0b159e-daae-4ef1-af7d-6441c656227b",
+    [string] $AzsK8SResourceGroup="k8s-jiadutest1",
+    [string] $SampleAPIModelLocation="https://raw.githubusercontent.com/Azure/aks-engine/master/examples/azure-stack/kubernetes-azurestack.json"
+)
+
+$Params2APIModelPropsKeyMapping = @{
+    "StampLocation" = "location"
+    "OrchestratorRelease" = "properties.orchestratorProfile.orchestratorRelease"
+    "OrchestratorVersion" = "properties.orchestratorProfile.orchestratorVersion"
+    "PortalURL" = "properties.customCloudProfile.portalURL"
+    "IdentitySystem" = "properties.customCloudProfile.identitySystem"
+    "DnsPrefix" = "properties.masterProfile.dnsPrefix"
+    "K8SAdminUser" = "properties.linuxProfile.adminUsername"
+    "K8SPublicKey" = "properties.linuxProfile.ssh.publicKeys[].keyData"
+    "K8SPClientId" = "properties.servicePrincipalProfile.clientId"
+    "K8SSPSecret" = "properties.servicePrincipalProfile.secret"
+}
+
+Write-Verbose -Message "Using Parameters: " -Verbose
+Write-Verbose -Message $($PSBoundParameters | ConvertTo-Json)  -Verbose
+
+# Installing tools
+$WorkingDir = "C:\k8stmp"
+if (-not(Test-Path -Path $WorkingDir -PathType Container)) {
+    New-Item -Path $WorkingDir -ItemType "directory" -Force
+}
+$AKSeVersion = "aks-engine-v0.55.4-windows-amd64"
+Invoke-WebRequest "https://aksereleases.blob.core.windows.net/public/$($AKSeVersion).zip" -OutFile "$WorkingDir\$($AKSeVersion).zip"
+Expand-Archive "$WorkingDir\$($AKSeVersion).zip" -DestinationPath "$WorkingDir" -Force
+$AKSeBinaryLocation = "$WorkingDir\$($AKSeVersion)\aks-engine.exe"
+
+$APIModelSampleLocation = "$WorkingDir\apimodel.json"
+$APIModelLocation = "$WorkingDir\apimodel-running.json"
+# Get the command name
+$CommandName = $PSCmdlet.MyInvocation.InvocationName;
+# Get the list of parameters for the command
+$ParameterList = (Get-Command -Name $CommandName).Parameters;
+
+# Using current folder as the running folder
+Invoke-WebRequest $SampleAPIModelLocation -OutFile $APIModelSampleLocation
+$apiModelJson = Get-Content $APIModelSampleLocation | ConvertFrom-Json
+# Composing the api model file
+foreach ($key in $ParameterList.Keys) {
+    $value = (Get-Variable $key -ErrorAction SilentlyContinue).Value
+    if ($Params2APIModelPropsKeyMapping.ContainsKey($key) -and ![string]::IsNullOrEmpty($value)) {
+        $jsonKey = $Params2APIModelPropsKeyMapping[$key]
+        Write-Host "Writing $value to $jsonKey" -ForegroundColor Yellow
+        $hierachies = $jsonKey.split(".")
+        $obj = $apiModelJson.PSObject.Properties
+        for($i=0; $i -lt ($hierachies.Count-1); $i++) {
+            $name = $hierachies[$i]
+            if ($name.EndsWith("[]")) {
+                # array, will pick item0
+                $obj = ($obj | Where-Object {$_.Name -eq $name.TrimEnd("[]")}).Value[0].PSObject.Properties
+            } else {
+                # object, will pick value
+                $obj = ($obj | Where-Object {$_.Name -eq $name}).Value.PSObject.Properties
+            }
+        }
+        $name = $hierachies[$hierachies.Count - 1]
+        if ($name.EndsWith("[]")) {
+            # array
+            ($obj | Where-Object {$_.Name -eq $name.TrimEnd("[]")}).Value = @($value)
+        } else {
+            # object
+            ($obj | Where-Object {$_.Name -eq $name}).Value = $value
+        }
+    }
+}
+Write-Host "Generating running api model..." -ForegroundColor Green
+$apiModelJson | ConvertTo-Json -Depth 100  | Out-File $APIModelLocation -Encoding ascii -Force
+
+Write-Host "Deploying K8S cluster by AKSe, using API model: $((Get-Item $APIModelLocation).FullName), using AKSe: $AKSeBinaryLocation"  -ForegroundColor Green
+if ([string]::IsNullOrEmpty($AKSeBinaryLocation) -or [string]::IsNullOrEmpty($AzsK8SSubscriptionId) -or [string]::IsNullOrEmpty($K8SPClientId) `
+    -or [string]::IsNullOrEmpty($K8SSPSecret) -or [string]::IsNullOrEmpty($StampLocation) -or [string]::IsNullOrEmpty($AzsK8SResourceGroup) `
+    -or [string]::IsNullOrEmpty($AzureEnv) -or [string]::IsNullOrEmpty($IdentitySystem)) {
+    Write-Host "Required parameter is null or empty, please check..." -ForegroundColor Green
+    exit 1
+}
+
+& $AKSeBinaryLocation deploy --subscription-id $AzsK8SSubscriptionId --client-id $K8SPClientId `
+    --client-secret $K8SSPSecret --location $StampLocation --resource-group $AzsK8SResourceGroup --api-model $((Get-Item $APIModelLocation).FullName) `
+    --azure-env $AzureEnv --identity-system $IdentitySystem
+
+# locate the k8s config file
+$KUBECONFIG = Get-Item "$WorkingDir\_output\$AzsK8SResourceGroup\kubeconfig\$($StampLocation)*.json"
+Write-Host "Using kube config file: $($kubeConfig.FullName)" -ForegroundColor Yellow
+[System.Environment]::SetEnvironmentVariable('KUBECONFIG', $KUBECONFIG,[System.EnvironmentVariableTarget]::Machine)
+
+exit 0
